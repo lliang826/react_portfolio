aac2e578d1b396ed8a87803342365ad69eacf60c
Lawrence Liang <[xlawrence.liangx@gmail.com]>
2022-04-26 22:51:09 -0700

    feat: created skeleton for homepage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import HomePage from './pages/HomePage';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<HomePage />} exact />
+            {/* <Route path="/about" element={<AboutPage />} exact />
+            <Route path="/list" element={<ToonListPage />} exact />
+            <Route path="/detail/:id" element={<ToonDetailPage />} exact />
+            <Route path="*" element={<NotFoundPage />} /> */}
+        </Routes>
+      </BrowserRouter>
     </div>
+
   );
 }
 
---
+++ src/pages/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const HomePage = () => (
+    <React.Fragment>
+    <h1>Test</h1>
+    <p>Testing</p>
+    </React.Fragment>
+);
+
+export default HomePage;
